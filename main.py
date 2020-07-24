acaddbf59a1014b3387aebc91aa81492b75963b7
Audrey Andoy <audreylandoy@gmail.com>
2020-07-24 18:17:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+name = input("Hello, what is your name? \n")
+print(f"Hello {name}! Let's start.")
+# print("Hello " + name + "! Let's start.")
+
+print()
+
+holiday = input("Enter a name of a holiday: ")
+place = input("Enter a place: ")
+hero = input("Enter a name of a hero: ")
+outfit = input("What would you like to wear? ")
+power = input("What super powers would you like to have?")
+
+print(f"Last {holiday}, I went to {place} and {hero} was walking around without their {outfit} and I couldn't tell they could {power}")
